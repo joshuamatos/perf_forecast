33a955972aefbd68886e0ec7d59dc152e7028a97
joshuamatos <joshua.matos@upr.edu>
2016-07-15 13:06:09 -0300

    Update perfexpert_forecast.c

--- perfexpert_forecast.c
+++ perfexpert_forecast.c
@@ -7,7 +7,6 @@
          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" \
          "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";*/
  // blah blah
- //
 //ESTOY BUSCANDO LA MANERA DE COMO PONER LO DE ARRIBA EN UN LOOP//
 
 #include <stdio.h>
@@ -19,10 +18,10 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    int i;
    for(i=0; i<argc; i++){
       printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
+   }//end of for
    printf("\n");
    return 0;
-}
+}//end of static int callback
 
 int main(int argc, char* argv[])
 {
@@ -41,8 +40,13 @@ int main(int argc, char* argv[])
       fprintf(stderr, "Opened database successfully\n"); //abracadabra
        }
 
-//SQLITE
+//Creating Table
+c_snprintf = snprintf(sql,4096,"CREATE TABLE database1.db (ID INT PRIMATY KEY NOT NULL UNIQUE,ARCHITECTURE INT NOT NULL
+,LOWER_BOUND REAL NOT NULL,UPPER_BOUND REAL NOT NULL,FORECAST INT NOT NULL);" 
+);//end of c_snprintf
+printf("Table Created Successfully")
 
+//SQLITE
   	 rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
    if( rc != SQLITE_OK ){
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
@@ -53,7 +57,7 @@ int main(int argc, char* argv[])
        }
 
 //THIS IS THE INFORMATION THAT IM GOING TO ASK THE USER FOR//
-int given_counter,age,c_snprintf,c_snprintf2,counter,id;
+int given_counter,age,c_snprintf,c_snprintf2,counter,id,edit_option;
 char name[35];
 
 //THIS PRINTF IS GOING TO DICTATE HOW BIG IS GOING TO BE THE DATABASE
@@ -63,20 +67,33 @@ printf("Enter de number of column you are going to add in the database\n");
 
 //THIS LOOP IS TO DO THE SAME THAT IS IN THE CORNER UP BUT DOING IT MANUALLY//
 //MUST INCLUDE A OPTION JUST IN CASE, THE INFORMATION INPUT IS WRONG//
+while{
+	c_snprintf = snprintf(sql,4096,"INSERT INTO PERFEXPERT_FORECAST %d,'%s',%d;",counter,name,age);
+	printf("Is the data correct?, please enter 0 if incorrect")
+	scanf("%d",&exit);
+	printf("%s\n",sql);
+	counter++;
+	if(edit_option==0)
+	{
+	//
+	}
+//   }(counter<=given_counter);
+     }(counter<=given_counter && edit_option!=0 );
+     
+   sqlite3_close(db);
+   return 0;
+}//end of int main
+//Chewie we are home...
+
+/*
 for (counter = 0; counter < given_counter; ++counter)
 {
 	// THIS IS GOING TO ASK FOR ALL THE SQL INFORMATION NECESSARY FOR THE NECESSARY COLUMNS 
-	//REMEBER TO INCLUDE ALL THE PRINTF//
 //######################################################################################//
 c_snprintf = snprintf(sql,4096,"INSERT INTO PERFEXPERT_FORECAST %d,'%s',%d;",counter,name,age);
 printf("%s\n",sql);
 //###############THIS##IS##INCORRECT###################################################//
 
-//NO SE SI ES & o el Pointer *
-          //"VALUES(&counter,'&names','&age','company_name',&salary)"
 //system("cls");THIS IS JUST IN CASE....
-}//Chewie we are home...
-   sqlite3_close(db);
-   return 0;
-//}
 }
+*/
