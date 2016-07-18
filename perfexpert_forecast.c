52c50e7b9ef05fe9b58f674433be97bc81c3dce0
joshuamatos <joshua.matos@upr.edu>
2016-07-17 23:13:33 -0300

    Update perfexpert_forecast.c

--- perfexpert_forecast.c
+++ perfexpert_forecast.c
@@ -27,9 +27,12 @@ int main(int argc, char* argv[])
 {
    sqlite3 *db;
    char *zErrMsg = 0;
-   int rc;
-   char sql[4096];
-
+   int rc,given_counter,age,a_snprintf,b_snprintf,c_snprintf2,counter,id,edit_option;
+   int architecture = 6;
+   float lower_bound,upper_bound,forecast;
+   char sql[4096],char name[35];
+   char excution_sql[] = "INSERT VALUE INTO";
+   
    /* Open database */
    rc =  perfexpert_database_connect(&db,"perf_forecasttable.db");
    if( rc ){
@@ -40,37 +43,44 @@ int main(int argc, char* argv[])
       fprintf(stderr, "Opened database successfully\n"); //abracadabra
        }
 
-//Creating Table
-c_snprintf = snprintf(sql,4096,"CREATE TABLE database1.db (ID INT PRIMATY KEY NOT NULL UNIQUE,ARCHITECTURE INT NOT NULL
+/*This will creat the table with certain arquitecture*/
+a_snprintf = snprintf(sql,4096,"CREATE TABLE database1.db (ID INT PRIMATY KEY NOT NULL UNIQUE,ARCHITECTURE INT NOT NULL
 ,LOWER_BOUND REAL NOT NULL,UPPER_BOUND REAL NOT NULL,FORECAST INT NOT NULL);" 
-);//end of c_snprintf
+);//end of a_snprintf
+
 printf("Table Created Successfully")
 
-//SQLITE
-  	 rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+
+	/*SQLITE*/
+  	rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+  	
    if( rc != SQLITE_OK ){
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
       	sqlite3_free(zErrMsg);
    }
+   
    else{
       fprintf(stdout, "Records created successfully\n");
        }
 
-//THIS IS THE INFORMATION THAT IM GOING TO ASK THE USER FOR//
-int given_counter,age,c_snprintf,c_snprintf2,counter,id,edit_option;
-char name[35];
+/*THIS PRINTF IS GOING TO DICTATE HOW BIG IS GOING TO BE THE DATABASE*/
+	printf("Enter de number of column you are going to insert in the database\n");
+		scanf("%d",&given_counter);
 
-//THIS PRINTF IS GOING TO DICTATE HOW BIG IS GOING TO BE THE DATABASE
-//sprintf & sscanf if it does not work//
-printf("Enter de number of column you are going to add in the database\n");
-	scanf("%d",&given_counter);
+/*This loop will insert one by one the values desire */
 
-//THIS LOOP IS TO DO THE SAME THAT IS IN THE CORNER UP BUT DOING IT MANUALLY//
 //MUST INCLUDE A OPTION JUST IN CASE, THE INFORMATION INPUT IS WRONG//
 while{
-	c_snprintf = snprintf(sql,4096,"INSERT INTO PERFEXPERT_FORECAST %d,'%s',%d;",counter,name,age);
-	printf("Is the data correct?, please enter 0 if incorrect")
-	scanf("%d",&exit);
+//	c_snprintf = snprintf(sql,4096,"'%s', %d,'%s',%d;",excution_sql,counter,name,age);
+	printf("Enter Lower_Bound data");
+	scanf("", );
+	printf("Enter Upper_Bound data");
+	scanf("", );
+	printf("Enter Forecast data")/* this will be given in the future but for reference*/
+	scanf("",)
+     /* printf("Is the data correct?, please enter 0 if incorrect")
+	scanf("%d",&exit); 
+     */
 	printf("%s\n",sql);
 	counter++;
 	if(edit_option==0)
@@ -78,14 +88,13 @@ while{
 	//
 	}
 //   }(counter<=given_counter);
-     }(counter<=given_counter && edit_option!=0 );
+//     }(counter<=given_counter && edit_option!=0 );
      
    sqlite3_close(db);
    return 0;
 }//end of int main
-//Chewie we are home...
 
-/*
+
 for (counter = 0; counter < given_counter; ++counter)
 {
 	// THIS IS GOING TO ASK FOR ALL THE SQL INFORMATION NECESSARY FOR THE NECESSARY COLUMNS 
